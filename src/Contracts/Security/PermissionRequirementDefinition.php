60d79d2e8abaf6d19d12c6da87bc1efcc0b2b19d
Mohamed Abdulalim <megyptm@gmail.com>
2026-05-01 23:53:46 +0300

    chore: stop tracking composer lock file

---
+++ src/Contracts/Security/PermissionRequirementDefinition.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @copyright   ©2026 Maatify.dev
+ * @Library     maatify/SharedCommon
+ * @Project     maatify:SharedCommon
+ * @author      Mohamed Abdulalim (megyptm) <mohamed@maatify.dev>
+ * @since       2026-05-01 23:42
+ * @see         https://www.maatify.dev Maatify.dev
+ * @link        https://github.com/Maatify/SharedCommon view Project on GitHub
+ * @note        Distributed in the hope that it will be useful - WITHOUT WARRANTY.
+ */
+
+declare(strict_types = 1);
+
